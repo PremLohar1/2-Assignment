7e211dc7ac6f14bfea2333fa2708d792b09d588b
Prem lohar <loharprem046@gmail.com>
2025-09-28 20:46:50 +0530

    Add files via upload

---
+++ As2T1.py
@@ -0,0 +1,11 @@
+#Assignment : 2
+
+#Task : 1
+#Chack Odd Even number 
+num=int(input('Enter The Number = '))
+print('\n')
+if num%2==0:
+    print('This is Even Number')
+else:
+    print('This Is Odd Number \n') 
+ 
---
+++ As2T2.py
@@ -0,0 +1,9 @@
+#Assignment : 2
+
+#Task : 2
+#Chack Sum 1 To 150
+ans = 0
+for i in range(1,51):
+    ans+=i
+ 
+print('The 1 To 50 Sum is = ',ans)
